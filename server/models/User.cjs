4ec0926eb0d2f27797ab714e4d784af4c8acce75
denikeev <script.edf@gmail.com>
2023-04-16 19:44:31 +0300

    add filename & lastName fields

--- server/models/User.cjs
+++ server/models/User.cjs
@@ -1,7 +1,7 @@
 // @ts-check
 
-const BaseModel = require('./BaseModel.cjs');
 const objectionUnique = require('objection-unique');
+const BaseModel = require('./BaseModel.cjs');
 const encrypt = require('../lib/secure.cjs');
 
 const unique = objectionUnique({ fields: ['email'] });
@@ -14,10 +14,16 @@ module.exports = class User extends unique(BaseModel) {
   static get jsonSchema() {
     return {
       type: 'object',
-      required: ['email', 'password'],
+      required: ['firstName', 'lastName', 'email', 'password'],
       properties: {
         id: { type: 'integer' },
-        email: { type: 'string', minLength: 1 },
+        firstName: { type: 'string', minLength: 1 },
+        lastName: { type: 'string', minLength: 1 },
+        email: {
+          type: 'string',
+          minLength: 1,
+          pattern: '^\\S+@\\S+\\.\\S+$',
+        },
         password: { type: 'string', minLength: 3 },
       },
     };
@@ -30,4 +36,4 @@ module.exports = class User extends unique(BaseModel) {
   verifyPassword(password) {
     return encrypt(password) === this.passwordDigest;
   }
-}
+};
